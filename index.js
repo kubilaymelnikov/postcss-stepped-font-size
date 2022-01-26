a1ff83c17ac7a78b2091d27742d368064ac50ffb
Kubilay Melnikov <melnikov@medienagenten.de>
2022-01-26 12:53:37 +0100

    feat: implement https://codepen.io/kpeksen/pen/WNoyJMP as postcss plugin

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /**
  * @type {import('postcss').PluginCreator}
  */
-module.exports = (opts = {}) => {
+module.exports = () => {
   // Work with options here
 
   return {
-    postcssPlugin: 'postcss-stepped-font-size',
+    // https://github.com/postcss/postcss/blob/main/docs/writing-a-plugin.md
+    postcssPlugin: "postcss-stepped-font-size",
     /*
     Root (root, postcss) {
       // Transform CSS AST here
@@ -18,14 +19,86 @@ module.exports = (opts = {}) => {
     }
     */
 
-    /*
     Declaration: {
-      color: (decl, postcss) {
-        // The fastest way find Declaration node if you know property name
-      }
-    }
-    */
-  }
-}
+      "stepped-font-size": (decl) => {
+        const splitValuAndUnit = (size) => ({
+          _: size,
+          value: parseFloat(/[0-9]+/.exec(size)[0]),
+          unit: /[a-z]+/i.exec(size)[0],
+        });
+        const selector = decl.parent.selector;
+        const fontSizes = /(.*?)\(/
+          .exec(decl.value)[1]
+          .replaceAll(" ", "")
+          .split(",")
+          .filter((_) => !!_)
+          .map(splitValuAndUnit);
 
-module.exports.postcss = true
+        const breakpoints = /\((.*?)\)/
+          .exec(decl.value)[1]
+          .replaceAll(" ", "")
+          .split(",")
+          .map(splitValuAndUnit);
+
+        const maxWidth = breakpoints[breakpoints.length - 1];
+        const minWidth = breakpoints.shift();
+        const maxFontSize = fontSizes[0];
+        const minFontSize = fontSizes[1];
+
+        decl.parent.before(`
+          ${selector} {
+            min-width: 0vw;
+            line-height: normal;
+          }
+        `);
+
+        decl.parent.before(`
+          @media (max-width: ${minWidth._}) {
+            ${selector} {
+              font-size: ${minFontSize._};
+            }
+          }
+        `);
+
+        var prevStep = minWidth;
+
+        breakpoints.forEach((breakpoint) => {
+          const targetStep = breakpoint;
+          let size =
+            Math.round(
+              (minFontSize.value +
+                ((maxFontSize.value - minFontSize.value) /
+                  (maxWidth.value - minWidth.value)) *
+                  (targetStep.value - minWidth.value)) *
+                10
+            ) / 10;
+
+          decl.parent.before(`
+            @media (min-width: ${prevStep._}) and (max-width: ${targetStep._}) {
+              ${selector} {
+                font-size: ${size}${minFontSize.unit};
+              }
+            }
+          `);
+
+          prevStep = targetStep;
+        });
+
+        decl.parent.before(`
+          @media (min-width: ${maxWidth._}) {
+            ${selector} {
+              font-size: ${maxFontSize._};
+            }
+          }
+        `);
+
+        const parent = decl.parent;
+
+        decl.remove();
+        if (!parent.nodes.length) parent.remove();
+      },
+    },
+  };
+};
+
+module.exports.postcss = true;
--- index.test.js
+++ index.test.js
@@ -1,17 +1,60 @@
-const postcss = require('postcss')
+const postcss = require("postcss");
 
-const plugin = require('./')
+const plugin = require("./");
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
+async function run(input, output, opts = {}) {
+  let result = await postcss([plugin(opts)]).process(input, {
+    from: undefined,
+  });
+  expect(result.css.replace(/(?:\r\n|\r|\n| )/g, "")).toEqual(
+    output.replace(/(?:\r\n|\r|\n| )/g, "")
+  );
+  expect(result.warnings()).toHaveLength(0);
 }
 
-/* Write tests here
-
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
-})
-
-*/
+it("does something", async () => {
+  await run(
+    `
+      a {
+        stepped-font-size: 80px, 20px, (576px, 768px, 992px, 1200px, 1400px);
+      }
+    `,
+    `
+      a {
+        min-width: 0vw;
+        line-height: normal;
+      }
+      @media (max-width: 576px) {
+        a {
+          font-size: 20px;
+        }
+      }
+      @media (min-width: 576px) and (max-width: 768px) {
+        a {
+          font-size: 34px;
+        }
+      }
+      @media (min-width: 768px) and (max-width: 992px) {
+        a {
+          font-size: 50.3px;
+        }
+      }
+      @media (min-width: 992px) and (max-width: 1200px) {
+        a {
+          font-size: 65.4px;
+        }
+      }
+      @media (min-width: 1200px) and (max-width: 1400px) {
+        a {
+          font-size: 80px;
+        }
+      }
+      @media (min-width: 1400px) {
+        a {
+          font-size: 80px;
+        }
+      }
+    `,
+    {}
+  );
+});
